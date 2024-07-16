9384f0e66659cdbafbbdca53245cce84ffce9aba
Bohyun Jung <orange.b.jung@gmail.com>
2024-07-17 02:15:58 +0900

    Initial commit

---
+++ blog/2024-07-14-first-post.md
@@ -0,0 +1,10 @@
+---
+slug: first-post
+title: 첫 번째 글
+---
+
+...
+
+<!-- truncate -->
+
++++
---
+++ docusaurus.config.ts
@@ -0,0 +1,66 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'bohyunjung.com',
+  favicon: 'img/favicon.ico',
+
+  url: 'https://bohyunjung.com',
+  baseUrl: '/',
+
+  organizationName: 'bohyunjung',
+  projectName: 'bohyunjung.github.io',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'ko',
+    locales: ['ko'],
+  },
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: false,
+        blog: {
+          routeBasePath: '/',
+          blogSidebarCount: 0,
+          showReadingTime: false,
+          blogListComponent: '@theme/BlogListPage',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      },
+    ],
+  ],
+
+  themeConfig: {
+    navbar: {
+      title: 'bohyunjung.com',
+      items: [
+        { to: '/about', label: 'About', position: 'right' },
+        {
+          to: 'https://www.linkedin.com/in/bohyun-jung/',
+          label: 'LinkedIn',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'light',
+      copyright: `© ${new Date().getFullYear()} Bohyun Jung. Built with <a href="https://docusaurus.io/" target="_blank">Docusaurus.</a>`,
+    },
+    prism: {
+      theme: prismThemes.github,
+    },
+    colorMode: {
+      disableSwitch: true,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
